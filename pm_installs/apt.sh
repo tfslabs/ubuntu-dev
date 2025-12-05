85b2d794efbe093c5000c8d9ddda8e119fab6322
Shiroi Neko <shiroineko.tfs@theflightsims.com>
2025-12-05 15:04:22 +0700

    Update docker-image.yml and apt.sh for optimization and additional packages

--- pm_installs/apt.sh
+++ pm_installs/apt.sh
@@ -32,7 +32,7 @@ apt install -y autotools-dev coreutils file locales tzdata \
     time tar xz-utils curl jq dbus xxd
 
 # Compression & archiving
-apt install -y p7zip-full p7zip-rar 7zip \
+apt install -y p7zip-full p7zip-rar 7zip 7zip-standalone \
     bzip2 lz4 unzip upx xorriso zip zsync zstd
 
 # Development tools & build dependencies
@@ -44,11 +44,12 @@ apt install -y libaom3 libalgorithm-diff-perl \
     libalgorithm-diff-xs-perl libalgorithm-merge-perl \
     libarchive13 libasan8 libatomic1 libbinutils libbrotli1 \
     libcc1-0 libc-dev-bin libc-devtools libc6 libc6-dev \
-    libcurl4 libde265-0 libdeflate0 libcrypt-dev libctf-nobfd0 \
-    libdpkg-perl libexpat1 libfakeroot libfile-fcntllock-perl \
-    libfl2 libfontconfig1 libfreetype6 libgbm-dev libgmp-dev \
-    libgmpxx4ldbl libgcc-12-dev libgcc-s1 libgomp1 libgd3 \
-    libgsl-dev libgssapi-krb5-2 libgtk-3-0 libheif1 libitm1 \
+    libc6-dev-*-cross libcurl4 libde265-0 libdeflate0 \
+    libcrypt-dev libctf-nobfd0 libdpkg-perl libexpat1 \
+    libfakeroot libfile-fcntllock-perl libfl2 libfontconfig1 \
+    libfreetype6 libgbm-dev libgmp-dev libgmpxx4ldbl \
+    libgcc-12-dev libgcc-s1 libgomp1 libgd3 libgsl-dev \
+    libgssapi-krb5-2 libgtk-3-0 libheif1 libitm1 \
     libicu74 libjbig0 libjsoncpp25 liblsan0 libmagic-dev \
     libmpc-dev libmpfr-dev libnghttp2-14 libnsl-dev libnsl2 \
     libnuma1 libpng16-16 libpsl5 librhash0 librtmp1 \
@@ -68,7 +69,7 @@ apt install -y iproute2 iputils-ping dnsutils net-tools \
 apt install -y gnupg2 haveged libnss3-tools pass openssl
 
 # Version control & SCM
-apt install -y git
+apt install -y mercurial subversion git
 
 # File & media processing
 apt install -y ffmpeg imagemagick mediainfo \
@@ -104,6 +105,9 @@ else
     echo "binutils-x86-64-gnu is not available for ARM64"
 fi
 
+# Installing ARM toolchains
+apt install -y gcc-arm-none-eabi gdb-arm-none-eabi binutils-arm-none-eabi
+
 # Installing LLVM toolchain
 apt install -y llvm llvm-dev llvm-runtime clangd lld lldb \
     libclang-dev libllvm14 llvm-14 llvm-14-dev llvm-14-runtime
@@ -129,6 +133,16 @@ apt install -y default-jdk \
 apt install -y \
     dotnet-sdk-8.0 aspnetcore-runtime-8.0 dotnet-runtime-8.0 \
     dotnet-sdk-9.0 aspnetcore-runtime-9.0 dotnet-runtime-9.0
+
+# Installing Rust programming language and toolchain
+apt install -y rustc cargo rust-doc rust-src rust-gdb rust-clippy rustfmt
+
+# Installing Android SDK command-line tools
+apt install -y adb android-sdk-platform-tools android-sdk-build-tools \
+    android-sdk-platform-tools-common fastboot
+
+# Installing Go programming language
+apt install -y golang golang-go golang-src
 
 # Installing Node.js and npm
 apt install -y nodejs npm yarn
