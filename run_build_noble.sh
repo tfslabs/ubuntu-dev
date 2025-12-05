ee39d64fc66f230cad55def2fc600c2de7ed2fbf
shiroinekotfs <shiroineko.tfs@theflightsims.com>
2025-12-05 16:30:41 +0700

    .

--- run_build_noble.sh
+++
@@ -1,9 +0,0 @@
-#/bin/sh
-
-set -euo pipefail
-
-cd "$(cd "$(dirname "$0")" && pwd)"
-
-for arch in amd64 arm64/v8; do
-    docker build --platform "linux/$arch" -f ./build_noble -t "theflightsims/ubuntu-dev:noble" .
-done
